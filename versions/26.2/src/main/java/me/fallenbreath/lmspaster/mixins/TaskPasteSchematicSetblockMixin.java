24a0a42cc88bdc7761d6a7770ad6e5532ab77071
ogxldn <71860758+ogxldn@users.noreply.github.com>
2026-08-25 17:07:29 +0100

    Align 26.2 mixin with modern paste implementation

--- versions/26.2/src/main/java/me/fallenbreath/lmspaster/mixins/TaskPasteSchematicSetblockMixin.java
+++ versions/26.2/src/main/java/me/fallenbreath/lmspaster/mixins/TaskPasteSchematicSetblockMixin.java
@@ -20,14 +20,19 @@
 
 package me.fallenbreath.lmspaster.mixins;
 
+import fi.dy.masa.litematica.config.Configs;
+import fi.dy.masa.litematica.scheduler.tasks.TaskPasteSchematicPerChunkBase;
 import fi.dy.masa.litematica.scheduler.tasks.TaskPasteSchematicPerChunkCommand;
-import fi.dy.masa.litematica.util.EntityUtils;
-import fi.dy.masa.malilib.util.position.IntBoundingBox;
+import fi.dy.masa.litematica.schematic.placement.SchematicPlacement;
+import fi.dy.masa.litematica.util.PasteNbtBehavior;
+import fi.dy.masa.malilib.util.position.LayerRange;
 import me.fallenbreath.lmspaster.LitematicaServerPasterMod;
 import me.fallenbreath.lmspaster.network.ClientNetworkHandler;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.commands.arguments.blocks.BlockStateParser;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtUtils;
 import net.minecraft.util.ProblemReporter;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -35,151 +40,242 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.level.storage.TagValueOutput;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import javax.annotation.Nullable;
-import java.util.Locale;
-import java.util.Queue;
+import java.util.Collection;
+import java.util.function.Consumer;
 
 @Mixin(TaskPasteSchematicPerChunkCommand.class)
-public abstract class TaskPasteSchematicSetblockMixin
+public abstract class TaskPasteSchematicSetblockMixin extends TaskPasteSchematicPerChunkBase
 {
-	@Shadow @Final protected Queue<String> queuedCommands;
-	@Shadow @Final protected String setBlockCommand;
-	@Shadow @Final protected String summonCommand;
-	@Shadow @Final protected String delayCommand;
-	@Shadow protected int maxCommandsPerTick;
-	@Shadow protected int sentCommandsThisTick;
-	@Shadow @Nullable protected IntBoundingBox currentBox;
+	@Shadow(remap = false) @Final protected String setBlockCommand;
 
-	@Shadow
-	protected abstract boolean shouldSetBlock(BlockState stateSchematic, BlockState stateClient);
+	@Unique
+	private ChunkAccess currentSchematicChunk;
 
-	@Inject(method = "pasteBlock", at = @At("HEAD"), cancellable = true, remap = false)
-	private void pasteBlockWithServerPaster(BlockPos pos, LevelChunk schematicChunk, ChunkAccess clientChunk, boolean ignoreLimit, CallbackInfo ci)
+	public TaskPasteSchematicSetblockMixin(Collection<SchematicPlacement> placements, LayerRange range, boolean changedBlocksOnly)
 	{
-		if (!ClientNetworkHandler.isServerPasterAvailable())
+		super(placements, range, changedBlocksOnly);
+	}
+
+	@Unique
+	private static final String CUSTOM_COMMAND_PREFIX = String.format("##%s##", LitematicaServerPasterMod.MOD_ID);
+
+	@ModifyVariable(
+			method = "generateFillVolumes",
+			at = @At(
+					value = "INVOKE",
+					target = "Lit/unimi/dsi/fastutil/longs/LongArrayList;clear()V"
+			),
+			ordinal = 0,
+			remap = false
+	)
+	private boolean forcedSetIgnoreBeFromFill(boolean ignoreBeFromFill)
+	{
+		return ignoreBeFromFill || ClientNetworkHandler.isServerPasterAvailable();
+	}
+
+	@Override
+	protected void sendCommand(String command, LocalPlayer player)
+	{
+		if (command.startsWith(CUSTOM_COMMAND_PREFIX))
 		{
-			return;
+			command = command.substring(CUSTOM_COMMAND_PREFIX.length());
+			if (command.charAt(0) != '/')
+			{
+				command = '/' + command;
+			}
+			ClientNetworkHandler.sendCommand(command);
 		}
-
-		BlockState stateSchematic = schematicChunk.getBlockState(pos);
-		BlockState stateClient = clientChunk.getBlockState(pos);
-		if (!this.shouldSetBlock(stateSchematic, stateClient))
+		else
 		{
-			ci.cancel();
-			return;
-		}
-
-		BlockEntity blockEntity = schematicChunk.getBlockEntity(pos);
-		if (blockEntity == null)
-		{
-			return;
-		}
-
-		CompoundTag tag = blockEntity.saveWithoutMetadata(schematicChunk.getLevel().registryAccess());
-		tag.remove("id");
-		tag.remove("x");
-		tag.remove("y");
-		tag.remove("z");
-
-		String stateString = BlockStateParser.serialize(stateSchematic);
-		String command = String.format(Locale.ROOT, "%s %d %d %d %s%s", this.setBlockCommand,
-				pos.getX(), pos.getY(), pos.getZ(), stateString, tag);
-
-		if (ClientNetworkHandler.canSendCommand(command))
-		{
-			LitematicaServerPasterMod.LOGGER.info("Pasting block {} at [{}, {}, {}] with nbt tag",
-					stateSchematic.getBlock().getName().getString(), pos.getX(), pos.getY(), pos.getZ());
-			this.queuedCommands.offer(command);
-			ci.cancel();
+			super.sendCommand(command, player);
 		}
 	}
 
-	@Inject(method = "summonEntity", at = @At("HEAD"), cancellable = true, remap = false)
-	private void summonEntityWithServerPaster(Entity entity, CallbackInfo ci)
+	@Unique
+	@Nullable
+	private String customCommand = null;
+
+	@Inject(method = "pasteBlock", at = @At("HEAD"), remap = false)
+	private void recordCurrentSchematicChunk(BlockPos pos, LevelChunk schematicChunk, ChunkAccess clientChunk, boolean ignoreLimit, CallbackInfo ci)
 	{
-		if (!ClientNetworkHandler.isServerPasterAvailable())
+		this.currentSchematicChunk = schematicChunk;
+	}
+
+	@Inject(
+			method = "queueSetBlockCommand(IIILnet/minecraft/world/level/block/state/BlockState;Ljava/util/function/Consumer;)V",
+			slice = @Slice(
+					from = @At(
+							value = "FIELD",
+							target = "Lfi/dy/masa/litematica/scheduler/tasks/TaskPasteSchematicPerChunkCommand;setBlockCommand:Ljava/lang/String;",
+							remap = false
+					)
+			),
+			at = @At(
+					value = "INVOKE",
+					target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V",
+					remap = false,
+					ordinal = 0
+			),
+			remap = false
+	)
+	private void useCustomLongChatPacketToPasteBlockNbtDirectly(int x, int y, int z, BlockState state, Consumer<String> commandHandler, CallbackInfo ci)
+	{
+		if (Configs.Generic.PASTE_NBT_BEHAVIOR.getOptionListValue() != PasteNbtBehavior.NONE)
 		{
 			return;
 		}
-
-		if (this.currentBox != null)
+		if (ClientNetworkHandler.isServerPasterAvailable())
 		{
-			int x = entity.getBlockX();
-			int y = entity.getBlockY();
-			int z = entity.getBlockZ();
-			if (x < this.currentBox.minX() || x > this.currentBox.maxX() ||
-					y < this.currentBox.minY() || y > this.currentBox.maxY() ||
-					z < this.currentBox.minZ() || z > this.currentBox.maxZ())
+			BlockEntity blockEntity = this.currentSchematicChunk.getBlockEntity(new BlockPos(x, y, z));
+			if (blockEntity != null)
 			{
-				ci.cancel();
-				return;
+				String stateString = BlockStateParser.serialize(state);
+				CompoundTag tag = blockEntity.saveWithoutMetadata(this.world.registryAccess());
+				tag.remove("id");
+				tag.remove("x");
+				tag.remove("y");
+				tag.remove("z");
+				String command = String.format("/%s %s %s %s %s%s", this.setBlockCommand, x, y, z, stateString, tag);
+				if (ClientNetworkHandler.canSendCommand(command))
+				{
+					LitematicaServerPasterMod.LOGGER.info("Pasting block {} at [{}, {}, {}] with nbt tag", state.getBlock().getName().getString(), x, y, z);
+					this.customCommand = command;
+				}
 			}
-		}
-
-		if (entity.getVehicle() != null)
-		{
-			ci.cancel();
-			return;
-		}
-
-		String id = EntityUtils.getEntityId(entity);
-		if (id == null)
-		{
-			return;
-		}
-
-		CompoundTag tag;
-		try (ProblemReporter.ScopedCollector reporter = new ProblemReporter.ScopedCollector(entity.problemPath(), LitematicaServerPasterMod.LOGGER))
-		{
-			TagValueOutput output = TagValueOutput.createWithContext(reporter, entity.registryAccess());
-			entity.saveWithoutId(output);
-			tag = output.buildResult();
-		}
-		tag.remove("UUIDMost");
-		tag.remove("UUIDLeast");
-		tag.remove("UUID");
-		tag.remove("Pos");
-		tag.remove("Dimension");
-
-		String command = String.format(Locale.ROOT, "%s %s %f %f %f %s", this.summonCommand, id,
-				entity.getX(), entity.getY(), entity.getZ(), tag);
-		if (ClientNetworkHandler.canSendCommand(command))
-		{
-			LitematicaServerPasterMod.LOGGER.info("Summoning entity {} with nbt tag", entity.getType().getDescription().getString());
-			this.queuedCommands.offer(command);
-			ci.cancel();
 		}
 	}
 
-	@Inject(method = "sendQueuedCommands", at = @At("HEAD"), cancellable = true, remap = false)
-	private void sendQueuedCommandsWithServerPaster(CallbackInfo ci)
+	@ModifyArg(
+			method = "queueSetBlockCommand(IIILnet/minecraft/world/level/block/state/BlockState;Ljava/util/function/Consumer;)V",
+			slice = @Slice(
+					from = @At(
+							value = "FIELD",
+							target = "Lfi/dy/masa/litematica/scheduler/tasks/TaskPasteSchematicPerChunkCommand;setBlockCommand:Ljava/lang/String;",
+							remap = false
+					)
+			),
+			at = @At(
+					value = "INVOKE",
+					target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V",
+					ordinal = 0,
+					remap = false
+			),
+			remap = false
+	)
+	private Object useCustomLongChatPacketToPasteBlockNbtDirectly_setBlock(Object obj)
 	{
-		if (!ClientNetworkHandler.isServerPasterAvailable())
-		{
-			return;
-		}
+		return this.useCustomLongChatIfPossible(obj);
+	}
 
-		while (this.sentCommandsThisTick < this.maxCommandsPerTick && !this.queuedCommands.isEmpty())
+	@ModifyArg(
+			method = "summonEntity",
+			at = @At(
+					value = "INVOKE",
+					target = "Ljava/util/Queue;offer(Ljava/lang/Object;)Z",
+					ordinal = 0,
+					remap = false
+			),
+			remap = false
+	)
+	private Object useCustomLongChatPacketToPasteEntityNbtDirectly(Object obj)
+	{
+		return this.useCustomLongChatIfPossible(obj);
+	}
+
+	@Unique
+	private Object useCustomLongChatIfPossible(Object obj)
+	{
+		if (this.customCommand != null && obj instanceof String)
 		{
-			String command = this.queuedCommands.poll();
-			if (command.equals(this.delayCommand))
+			obj = CUSTOM_COMMAND_PREFIX + this.customCommand;
+			this.customCommand = null;
+		}
+		return obj;
+	}
+
+	@Unique
+	private Entity currentEntity;
+	@Unique
+	private boolean cancelThisEntityPaste;
+
+	@Inject(method = "summonEntity", at = @At("HEAD"), remap = false)
+	private void recordCurrentEntity(Entity entity, CallbackInfo ci)
+	{
+		this.currentEntity = entity;
+	}
+
+	@ModifyVariable(
+			method = "summonEntity",
+			at = @At(
+					value = "STORE",
+					target = "Ljava/lang/String;format(Ljava/util/Locale;Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;",
+					ordinal = 0,
+					remap = false
+			),
+			ordinal = 1,
+			remap = false
+	)
+	private String useCustomLongChatPacketToPasteEntityNbtDirectly(String baseCommand)
+	{
+		this.cancelThisEntityPaste = false;
+		if (ClientNetworkHandler.isServerPasterAvailable() && this.currentEntity != null)
+		{
+			if (this.currentEntity.getVehicle() != null)
 			{
-				this.sentCommandsThisTick = this.maxCommandsPerTick;
+				this.cancelThisEntityPaste = true;
+				return baseCommand;
 			}
-			else
+
+			CompoundTag tag;
+			try (ProblemReporter.ScopedCollector logging = new ProblemReporter.ScopedCollector(this.currentEntity.problemPath(), LitematicaServerPasterMod.LOGGER))
 			{
-				ClientNetworkHandler.sendCommand(command);
-				++this.sentCommandsThisTick;
+				TagValueOutput view = TagValueOutput.createWithContext(logging, this.currentEntity.registryAccess());
+				this.currentEntity.save(view);
+				tag = view.buildResult();
+			}
+
+			tag.remove("UUID");
+			tag.remove("Pos");
+			tag.remove("Dimension");
+
+			String tagString = NbtUtils.toPrettyComponent(tag).getString();
+			String command = baseCommand + " " + tagString;
+			if (ClientNetworkHandler.canSendCommand(command))
+			{
+				LitematicaServerPasterMod.LOGGER.info("Summoning entity {} with nbt tag", this.currentEntity.getType().getDescription().getString());
+				this.customCommand = command;
 			}
 		}
+		return baseCommand;
+	}
 
-		ci.cancel();
+	@Inject(
+			method = "summonEntity",
+			at = @At(
+					value = "INVOKE",
+					target = "Ljava/util/Queue;offer(Ljava/lang/Object;)Z",
+					remap = false
+			),
+			remap = false,
+			cancellable = true
+	)
+	private void cancelNullCommand(Entity entity, CallbackInfo ci)
+	{
+		if (this.cancelThisEntityPaste)
+		{
+			ci.cancel();
+		}
 	}
 }
